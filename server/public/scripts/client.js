7b5286ef198e6f61d4d146011d2c7b28fa372d85
Jennifer Alexander <jenny_alexander@icloud.com>
2021-09-22 14:54:35 -0500

    Completed AJAX intro code-along

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,6 +4,7 @@ $( document ).ready( onReady );
 
 function onReady () {
     getMessages();
+    $( '#sendMessageButton' ).on( 'click', sendMessage );
 }
 
 function getMessages() {
@@ -15,9 +16,44 @@ function getMessages() {
     }).then( function( response ){
         // if successful
         console.log( 'back from server GET successful ', response );
+        //target output element
+        let el = $( '#messagesOut' );
+        //empty
+        el.empty();
+        //loop through array
+        for ( let i = 0; i < response.length; i++ ){
+            el.append(
+                //'<li>' + response[i].author + ' : ' + response[i].text + '</li>'
+                `<li><strong>${response[i].author}</strong> : ${response[i].text} </li>`
+            );
+        }
+
     }).catch( function( error ) {
         // got an error
         alert( 'problem!' );
         console.log( error );
     })
+}
+
+function sendMessage() {
+    // get user input & store in an object
+    let objectToSend = {
+        author: $( '#authorIn' ).val(),
+        text: $( '#textIn').val()
+    }
+    console.log( 'sending:', objectToSend );
+    // make AJAX post with the object
+    $.ajax({
+        method: 'POST',
+        url: '/messages',
+        data: objectToSend //must have data to send in a POST
+    }).then( function( response ) {
+        // if successful, update DOM
+        getMessages();
+    }).catch( function( error ) {
+        alert( error );
+        console.log( error );
+    })
+    
+    // catch any errors
 }
--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
 // requires
 const express = require( 'express' );
 const app = express();
+/// - NEEDED for POST - ///
+const bodyParser = require( 'body-parser' );
 
 // uses
 // server static files
 app.use( express.static( 'server/public' ) );
+/// - NEEDED for POST - ///
+app.use( bodyParser.urlencoded( { extended: true } ) );
 
 // globals
 const port = 5000;
 //test data
-let messages = [ {
-    author: 'Jen',
-    text: 'testing'
-}];
+let messages = [];
 
 // spin up server
 app.listen( port, ()=>{
@@ -23,4 +24,10 @@ app.listen( port, ()=>{
 app.get( '/messages', ( req, res )=>{
     console.log( '/messages GET hit' );
     res.send( messages );
+})
+
+app.post( '/messages', ( req, res )=>{
+    console.log( '/messages POST hit:', req.body );
+    messages.push( req.body );
+    res.send( 200 ); //generic "OK", 201 "Created"
 })
